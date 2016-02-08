3f3e1d0d513fa3dd9ce551b0ca261a6cf9186524
vaish-jain <jain.vaish94@gmail.com>
2016-02-08 17:10:11 +0530

    two loc commented

--- src/polyabs/PolyAbs.java
+++ src/polyabs/PolyAbs.java
@@ -34,8 +34,8 @@ public class PolyAbs {
         b=(abc)check(a);
         System.out.println(b.x);
         //comment
-        System.out.println(b.x);
-        System.out.println(b.x);
+        //System.out.println(b.x);
+        //System.out.println(b.x);
     }
     static class abc
     {
